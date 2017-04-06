a876f37745cdaaf5e91ae2f938b8b9c83d9aecc7
pavithB <pavithbuddhima@gmail.com>
2017-04-06 11:15:59 +0530

    done with ui in ceate appoinment ( not finalize )

--- app/src/main/java/com/pavithbuddhima/myschedule/CalenderMain.java
+++ app/src/main/java/com/pavithbuddhima/myschedule/CalenderMain.java
@@ -7,11 +7,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class CalenderMain extends AppCompatActivity implements View.OnClickListener {
 
 
     Button createNew ;
+    Button viewEdit ;
+    Button move ;
+    Button delete ;
+    CalendarView datePicker;
+
 
 
     @Override
@@ -20,8 +27,35 @@ public class CalenderMain extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_calender_main);
 
         createNew = (Button) findViewById(R.id.create);
+        viewEdit = (Button) findViewById(R.id.viewedit);
+        move = (Button) findViewById(R.id.move);
+        delete = (Button) findViewById(R.id.delete);
+        datePicker = (CalendarView) findViewById(R.id.pickdate);
+
 
         createNew.setOnClickListener(this);
+        viewEdit.setOnClickListener(this);
+        move.setOnClickListener(this);
+        delete.setOnClickListener(this);
+
+
+
+
+
+
+        datePicker.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override public void onSelectedDayChange(
+                    CalendarView view,
+                    int          year,
+                    int          month,
+                    int          dayOfMonth ) {
+
+                Toast.makeText(getApplicationContext(), ""+year+ " / " + (month+1) + " / " + dayOfMonth+" selected", Toast.LENGTH_SHORT).show();
+
+            }
+
+
+    });
 
     }
 
@@ -34,10 +68,30 @@ public class CalenderMain extends AppCompatActivity implements View.OnClickListe
                 Intent createAppoint = new Intent(this,CreateAppoinment.class);
                 startActivity(createAppoint);
 
+                break;
+            case R.id.move :
+
+                break;
+
+            case R.id.viewedit :
+
+                break;
+
+            case R.id.delete :
+
+                break;
+
+            case R.id.searchView :
+
+                break;
 
         }
 
     }
+
+
+
+
 
 
 }
