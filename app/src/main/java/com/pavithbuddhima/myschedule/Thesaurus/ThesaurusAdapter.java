77c34f56772f014dfac63350a556267b72a2dce0
pavithB <pavithbuddhima@gmail.com>
2017-05-09 03:02:26 +0530

    double checking the project

--- app/src/main/java/com/pavithbuddhima/myschedule/Thesaurus/ThesaurusAdapter.java
+++ app/src/main/java/com/pavithbuddhima/myschedule/Thesaurus/ThesaurusAdapter.java
@@ -14,10 +14,9 @@ import com.pavithbuddhima.myschedule.R;
 import java.util.List;
 
 
-
 public class ThesaurusAdapter extends ArrayAdapter<Synonym> {
 
-    TextView categoryTV , synonymsTV;
+    TextView categoryTV, synonymsTV;
 
 
     public ThesaurusAdapter(Context context, int textViewResourceId, List<Synonym> synonyms) {
@@ -25,21 +24,20 @@ public class ThesaurusAdapter extends ArrayAdapter<Synonym> {
     }
 
     /**
-     *
      * This method will create the rows for the list view
      */
     @Override
-    public View getView(int pos, View convertView, ViewGroup parent){
-        RelativeLayout row = (RelativeLayout)convertView;
-        if(null == row){
+    public View getView(int pos, View convertView, ViewGroup parent) {
+        RelativeLayout row = (RelativeLayout) convertView;
+        if (null == row) {
             //No recycled View, we have to inflate one.
-            LayoutInflater inflater = (LayoutInflater)parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            row = (RelativeLayout)inflater.inflate(R.layout.list_row_thesaurus, null);
+            LayoutInflater inflater = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = (RelativeLayout) inflater.inflate(R.layout.list_row_thesaurus, null);
         }
 
         //initializing the two text views
-        categoryTV = (TextView)row.findViewById(R.id.categoryTextView);
-        synonymsTV = (TextView)row.findViewById(R.id.synonymsTextView);
+        categoryTV = (TextView) row.findViewById(R.id.categoryTextView);
+        synonymsTV = (TextView) row.findViewById(R.id.synonymsTextView);
 
 
         //Set the resulting synonym category and synonyms in the TextViews
